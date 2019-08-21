1e6ea1a2215a804d7597ebb4fa8ac256aa3e44bc
yuceff28 <yuceff28@localhost.localdomain>
2019-08-21 09:26:59 +0300

    update

--- example.py
+++ example.py
@@ -1,16 +1,16 @@
 from arpyshell import ArPyShell
 
-bash = ArPyShell(shell="bash -c ")
-print(bash.ls("~/vlc.py ",output=True,utf8=False))
-print(bash.ls("~/vlc.py ",output=True,utf8=True))
+shell = ArPyShell()
+print(shell.ls("~/vlc.py ",output=True,utf8=False,shell="bash -c "))
+print(shell.ls("~/vlc.py ",output=True,utf8=True))
 
 
-print(bash.ls("~/vlc.py ",output=True,utf8=False))
-print(bash.ls("~/vlc.py ",output=False,utf8=True))
+print(shell.ls("~/vlc.py ",output=True,utf8=False))
+print(shell.ls("~/vlc.py ",output=False,utf8=True))
 
-print(bash.ls("~/vlc ",output=True))
-print(bash.ls("~/vlc ",output=False))
+print(shell.ls("~/vlc ",output=True))
+print(shell.ls("~/vlc ",output=False))
 
-print(bash.ls("fortest",output=True))
-print(bash.mkdir("-p fortest",output=True))
-print(bash.ls("fortest ",output=False))
+print(shell.ls("fortest",output=True))
+print(shell.mkdir("-p fortest",output=True))
+print(shell.ls("fortest ",output=False))
